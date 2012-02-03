f21db18fd0c33a1ff7a6b02f9a28a4c219782b8e
joshua.lipson@gmail.com <joshua.lipson@gmail.com@8adab459-3ef9-ba4b-f0fe-8299dcf77957>
2012-02-03 00:35:39 +0000

    Fixing SVN.

--- trunk/317 deob/compile.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Compiling...
-cd src
-"C:\Program Files\Java\jdk1.6.0_25\bin\javac.exe" -cp . -d ../bin/ ./*java
-pause
--- trunk/317 deob/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Client
-cd bin
-java client 10 0 highmem members 32
-pause
